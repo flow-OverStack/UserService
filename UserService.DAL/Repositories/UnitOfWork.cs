7df5131b40af14adae6921366730b52258491541
dotneteeer <447599489marat@gmail.com>
2025-01-19 11:10:34 +0300

    Unnecessary properties removed

--- UserService.DAL/Repositories/UnitOfWork.cs
+++ UserService.DAL/Repositories/UnitOfWork.cs
@@ -13,17 +13,12 @@ public class UnitOfWork : IUnitOfWork
     {
         _context = context;
         Users = users;
-        Roles = roles;
         UserRoles = userRoles;
     }
 
 
     public IBaseRepository<User> Users { get; set; }
-    public IBaseRepository<Role> Roles { get; set; }
-
     public IBaseRepository<UserRole> UserRoles { get; set; }
-    public IBaseRepository<Badge> Badges { get; set; }
-    public IBaseRepository<UserBadge> UserBadges { get; set; }
 
     public async Task<IDbContextTransaction> BeginTransactionAsync()
     {
--- UserService.Domain/Interfaces/Repositories/IUnitOfWork.cs
+++ UserService.Domain/Interfaces/Repositories/IUnitOfWork.cs
@@ -8,13 +8,7 @@ public interface IUnitOfWork : IStateSaveChanges
 {
     IBaseRepository<User> Users { get; set; }
 
-    IBaseRepository<Role> Roles { get; set; }
-
     IBaseRepository<UserRole> UserRoles { get; set; }
-
-    IBaseRepository<Badge> Badges { get; set; }
-
-    IBaseRepository<UserBadge> UserBadges { get; set; }
 
     Task<IDbContextTransaction> BeginTransactionAsync();
 }
