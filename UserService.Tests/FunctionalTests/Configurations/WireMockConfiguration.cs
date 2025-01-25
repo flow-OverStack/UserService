21abb80b94a6db4bfd0fc583d90a60d0e4d4bd03
dotneteeer <447599489marat@gmail.com>
2025-01-25 09:54:52 +0300

    Metadata endpoint set up

---
+++ UserService.Tests/FunctionalTests/Configurations/WireMockConfiguration.cs
@@ -0,0 +1,66 @@
+using System.Reflection;
+using WireMock.RequestBuilders;
+using WireMock.ResponseBuilders;
+using WireMock.Server;
+
+namespace UserService.Tests.FunctionalTests.Configurations;
+
+public static class WireMockConfiguration
+{
+    private const int Port = 5001;
+    private const string RealmName = "flowOverStack";
+
+    private const string FunctionalTestsDirectoryName = "FunctionalTests";
+    private const string ResponsesDirectoryName = "TestServerResponses";
+
+    public static WireMockServer GetServer()
+    {
+        var server = WireMockServer.Start(Port);
+
+
+        server.Given(Request.Create().WithPath($"/realms/{RealmName}/.well-known/openid-configuration").UsingGet())
+            .RespondWith(Response.Create()
+                .WithHeader("Content-Type", "application/json")
+                .WithBody(GetMetadata()).WithSuccess());
+
+        return server;
+    }
+
+    private static string GetMetadata()
+    {
+        const string metadataFileName = "MetadataResponse.json";
+        return GetResponse(metadataFileName);
+    }
+
+    private static string GetResponse(string fileName)
+    {
+        var projectDirectory = Directory.GetParent(AppContext.BaseDirectory); //path to runtime directory
+
+        var currentProjectName =
+            string.Join('.',
+                Assembly.GetExecutingAssembly().GetName().Name!.Split('.')
+                    .Take(2)); //name of current project if naming is of type '<AppName>.<DirectoryName>' (e. g. 'UserService.Api')
+
+        var filePath = GetPath(projectDirectory!, currentProjectName, fileName);
+
+        var response = File.ReadAllText(filePath);
+
+        return response;
+    }
+
+    private static string GetPath(DirectoryInfo runtimeDirectory, string currentProjectName, string fileName)
+    {
+        var currentProjectDirectory = runtimeDirectory;
+
+        while (currentProjectDirectory.Name != currentProjectName)
+            currentProjectDirectory =
+                currentProjectDirectory.Parent!; //getting path to current project directory from runtime directory
+
+
+        var filePath = Path.Combine(currentProjectDirectory.FullName, FunctionalTestsDirectoryName,
+            ResponsesDirectoryName,
+            fileName);
+
+        return filePath;
+    }
+}
