8ffcc5522a066aff9608aa90d085c79f1ed2cb9f
dotneteeer <447599489marat@gmail.com>
2025-02-17 20:30:12 +0300

    Logs updated

--- UserService.DAL/ApplicationDbContext.cs
+++ UserService.DAL/ApplicationDbContext.cs
@@ -2,15 +2,16 @@ using System.Reflection;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using UserService.DAL.Interceptors;
+using ILogger = Serilog.ILogger;
 
 namespace UserService.DAL;
 
-public sealed class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options, ILoggerFactory loggerFactory)
+public sealed class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options, ILogger logger)
     : DbContext(options)
 {
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseLoggerFactory(loggerFactory);
+        optionsBuilder.LogTo(logger.Information, LogLevel.Information);
         optionsBuilder.AddInterceptors(new DateInterceptor());
     }
 
