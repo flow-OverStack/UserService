eeac3a5a3f15e83f680e5017760f673179b5a776
dotneteeer <447599489marat@gmail.com>
2025-02-26 12:26:55 +0300

    Test ConnectionStrings:PostgresSQL specified

--- UserService.Tests/FunctionalTests/Base/FunctionalTestWebAppFactory.cs
+++ UserService.Tests/FunctionalTests/Base/FunctionalTestWebAppFactory.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Options;
@@ -51,6 +52,18 @@ public class FunctionalTestWebAppFactory : WebApplicationFactory<Program>, IAsyn
 
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
+        builder.ConfigureAppConfiguration((_, config) =>
+        {
+            var testConfig = new ConfigurationBuilder()
+                .AddInMemoryCollection(new Dictionary<string, string>
+                {
+                    { "ConnectionStrings:PostgresSQL", _userServicePostgreSql.GetConnectionString() }
+                }!)
+                .Build();
+
+            config.AddConfiguration(testConfig);
+        });
+
         builder.ConfigureTestServices(services =>
         {
             services.RemoveAll(typeof(DbContextOptions<ApplicationDbContext>));
