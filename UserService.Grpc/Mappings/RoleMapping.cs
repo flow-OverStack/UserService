58470327300383569a769d4071193e8117dfeb7d
dotneteeer <447599489marat@gmail.com>
2025-03-08 11:51:43 +0300

    Users removed from Role in UserService.proto

--- UserService.Grpc/Mappings/RoleMapping.cs
+++ UserService.Grpc/Mappings/RoleMapping.cs
@@ -6,9 +6,6 @@ public class RoleMapping : Profile
 {
     public RoleMapping()
     {
-        CreateMap<Domain.Entity.Role, Role>()
-            //does not cause the stack overflow
-            .ForMember(x => x.Users, x => x.Ignore())
-            .ReverseMap();
+        CreateMap<Domain.Entity.Role, Role>().ReverseMap();
     }
 }
