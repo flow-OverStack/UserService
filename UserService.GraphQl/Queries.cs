1c8af63a2779d3f54b9f8a3b0c3513400b8c6e4e
dotneteeer <447599489marat@gmail.com>
2025-03-08 10:05:55 +0300

    GraphQl get by id query added

--- UserService.GraphQl/Queries.cs
+++ UserService.GraphQl/Queries.cs
@@ -19,6 +19,19 @@ public class Queries
         return result.Data;
     }
 
+    [GraphQLDescription("Returns a user by its id")]
+    [UseFiltering]
+    [UseSorting]
+    public async Task<User> GetUser(long id, [Service] IGetUserService userService)
+    {
+        var result = await userService.GetUserByIdAsync(id);
+
+        if (!result.IsSuccess)
+            throw GraphQlExceptionHelper.Get(result.ErrorMessage!);
+
+        return result.Data;
+    }
+
 
     [GraphQLDescription("Returns a list of all roles")]
     [UseFiltering]
