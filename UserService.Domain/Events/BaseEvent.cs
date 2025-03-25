268251ef9e8758fbc71d95627db5829a596093f7
dotneteeer <447599489marat@gmail.com>
2025-03-25 09:39:41 +0300

    Code reformatted

--- UserService.Domain/Events/BaseEvent.cs
+++ UserService.Domain/Events/BaseEvent.cs
@@ -3,6 +3,7 @@ namespace UserService.Domain.Events;
 public class BaseEvent
 {
     public Guid EventId { get; set; }
+
     public string EventType { get; set; }
 
     public long UserId { get; set; }
