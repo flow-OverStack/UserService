f4ed9ec699292964a769c324e5f4b065b7aaab79
dotneteeer <447599489marat@gmail.com>
2025-05-18 11:28:38 +0300

    Migration for fix Role id sequence added

---
+++ UserService.DAL/Migrations/20250518082534_FixRoleIdSequence.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UserService.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixRoleIdSequence : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("""
+                                    SELECT setval(pg_get_serial_sequence('"Role"', 'Id'), (SELECT MAX("Id") FROM public."Role"));
+                                """);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
