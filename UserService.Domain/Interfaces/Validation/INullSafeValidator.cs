4702b1000b993f0fc90ec952a2c5e1d29dcff59d
dotneteeer <447599489marat@gmail.com>
2025-05-31 20:41:19 +0300

    Comment updated

--- UserService.Domain/Interfaces/Validation/INullSafeValidator.cs
+++ UserService.Domain/Interfaces/Validation/INullSafeValidator.cs
@@ -3,7 +3,7 @@ namespace UserService.Domain.Interfaces.Validation;
 public interface INullSafeValidator<in T>
 {
     /// <summary>
-    ///     Returns the given instance if valid; otherwise returns a fallback valid instance of T.
+    ///    Checks if instance is null and the validates it
     /// </summary>
     /// <param name="instance"></param>
     /// <param name="errorMessages"></param>
