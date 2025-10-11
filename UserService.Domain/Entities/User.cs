ceb1fab28c8f1cb1c58e3c131cf2ae1308949248
dotneteeer <447599489marat@gmail.com>
2025-10-11 10:33:44 +0300

    Unnecessary removed

--- UserService.Domain/Entities/User.cs
+++ UserService.Domain/Entities/User.cs
@@ -7,13 +7,10 @@ public class User : IEntityId<long>, IAuditable
     public string IdentityId { get; set; }
     public string Username { get; set; }
     public string Email { get; set; }
-    public DateTime LastLoginAt { get; set; } //is updated at successful login at token update  
+    public DateTime LastLoginAt { get; set; }
     public int Reputation { get; set; }
-
     public int ReputationEarnedToday { get; set; }
-
     public List<Role> Roles { get; set; }
-
     public DateTime CreatedAt { get; set; }
     public long Id { get; set; }
 }
